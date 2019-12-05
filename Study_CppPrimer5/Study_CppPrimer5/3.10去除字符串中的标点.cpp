08359a49398fc2b7415637bc69933fa96db4474f
Zenera0301 <847467911@qq.com>
2019-12-05 10:08:59 +0800

    去除字符串中的标标点

---
+++ Study_CppPrimer5/Study_CppPrimer5/3.10去除字符串中的标点.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string s;
+	getline(cin, s);
+	for (auto c : s){
+		if (!ispunct(c)){
+			cout << c ;
+		}
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
